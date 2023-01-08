796e5e959d364f4da1cf1c6b8d3ba58285d643c0
66hakeem <66hakeem@gmail.com>
2023-01-08 02:34:36 +0000

    adds css and assets

--- routes/web.php
+++ routes/web.php
@@ -13,10 +13,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-/*Route::get('/', function () {
-    return view('welcome');
-});*/
-
-Route::get('/second', function () {
-    return view('second');
+Route::get('/', function () {
+    return view('index');
 });
